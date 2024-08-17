0bd4d87a9a60b22c0c02d8ef9724077bbd34759b
Yukie765 <yliya-kate@mail.ru>
2024-08-18 02:11:56 +0500

    Add files via upload

---
+++ VarTask.py
@@ -0,0 +1,6 @@
+count_of_complete_tasks = 12
+count_of_spent_hours = 1.5
+course_name = 'Python'
+time_for_one_task = count_of_spent_hours/count_of_complete_tasks
+
+print(f'Курс: {course_name}, всего задач: {count_of_complete_tasks}, затрачено часов: {count_of_spent_hours}, среднее время выполнения {time_for_one_task} часа')
